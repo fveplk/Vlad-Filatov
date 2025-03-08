4d99e85c06e27b7c747544634801773927da75b0
Vlad <fuzz-exempt-6d@icloud.com>
2025-03-09 00:22:39 +0300

    Delete python directory

--- python/chislo s isklycheniem.py
+++
@@ -1,6 +0,0 @@
-import random
-numbers = (1,2,3,4,5,6,7,8,9,10)
-
-weights = [1,1,1,3,1,1,1,3,1,1]
-random_numbers = random.choices (numbers, weights=weights, k=3) 
-print (random_numbers)
--- python/fuck.py
+++
@@ -1,5 +0,0 @@
-import random
-stat = 1
-end = 100
-
-exlude = [5]
